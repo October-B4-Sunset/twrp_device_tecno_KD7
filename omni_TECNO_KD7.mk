aa3f7dc71ff1351407ad2572dc2868e7443570d8
Col-Serra <79519115+Col-Serra@users.noreply.github.com>
2022-04-18 08:53:37 +0100

    Update omni_TECNO_KD7.mk

--- omni_TECNO_KD7.mk
+++ omni_TECNO_KD7.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/tecno/TECNO_KD7/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := TECNO_KD7
